f2e8055a1352d479c8e059b7152506f5075793b0
snzaramba <nzsamuellk@gmail.com>
2018-01-20 15:57:38 +0200

    first push

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+var path = require('path');
+
+app.get('/', (req, res) => {
+   res.sendFile(path.join(__dirname + '/view.html'));
+});
+
+app.listen(3000, () => {
+  console.log('Web App running on port 3000!');
+});
