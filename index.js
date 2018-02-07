ef99be72f30004bdd7f5e48ae16e6a188e9e9e8a
Rene Kabagamba <renekabagamba@gmail.com>
2018-02-07 16:41:20 +0200

    Fixed the post request - now receiving directions

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const app = express();
 var path = require('path');
-var fs = require("fs") 
-var bodyParser = require("body-parser")
+var fs = require("fs");
+var bodyParser = require("body-parser");
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
 app.get('/', (req, res) => {
    res.sendFile(path.join(__dirname + '/show.html'));
 });
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 app.post('/addDirection', function(req, res) {
-	console.log("here " + JSON.stringify(req.body, null, 2))
-  res.send('You sent the name "' + req.body + '".');
+	console.log("here: " + req.body.instr)
+	//res.send('You sent the name "' + req.body + '".');
 });
 
 //fs.writeFile("datafile.txt", dat)	
